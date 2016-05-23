2818c2c74a9c0313feb7da2c3ef4af46a70981ed
Dmitry Davydov <dvdvdmt@gmail.com>
2016-05-23 23:16:32 +0500

    - db schema and tables creation

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Question(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField()
+    text = models.TextField()
+    rating = models.IntegerField()
     added_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey('User')
-    likes = models.ManyToManyField('User')
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(User)
 
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now=True)
-    question = models.ForeignKey('Question')
-    author = models.ForeignKey('User')
-
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User, related_name="answer_author")
